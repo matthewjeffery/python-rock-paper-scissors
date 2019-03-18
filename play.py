79fbfda0b69961f2b662856c48669b69166ba82e
Matthew Jeffery <matt_jeffery28@hotmail.co.uk>
2019-03-18 22:36:41 +0000

    Add score bar chart function and output

--- play.py
+++ play.py
@@ -50,6 +50,8 @@ class RockPaperScissors(object):
     def check_score(self):
         if self.args.score:
             self.display_score()
+            print("\n")
+            self.display_score_chart()
             exit()
 
     def display_score(self):
@@ -83,6 +85,39 @@ class RockPaperScissors(object):
             ]
         ]
         print(tabulate(score_table, score_table_headers, tablefmt="psql"))
+
+    def display_score_chart(self):
+        data = [
+            ('Wins', self.wins),
+            ('Loses', self.loses),
+            ('Draws', self.draws)
+        ]
+
+        max_value = max(count for _, count in data)
+        score_table_width = 38
+        increment = max_value / score_table_width
+
+        longest_label_length = max(len(label) for label, _ in data)
+
+        for label, count in data:
+            # The ASCII block elements come in chunks of 8, so we work out how
+            # many fractions of 8 we need.
+            # https://en.wikipedia.org/wiki/Block_Elements
+            bar_chunks, remainder = divmod(int(count * 8 / increment), 8)
+
+            # First draw the full width chunks
+            bar = '█' * bar_chunks
+
+            # Then add the fractional part. The Unicode code points for
+            # block elements are (8/8), (7/8), (6/8), etc. so we need to
+            # work backwards.
+            if remainder > 0:
+                bar += chr(ord('█') + (8 - remainder))
+
+            # If the bar is empty, add a left one-eighth block
+            bar = bar or '▏'
+
+            print(f'{label.rjust(longest_label_length)}: {bar}')
 
     def get_attack_method(self, choice: int):
         name = {
